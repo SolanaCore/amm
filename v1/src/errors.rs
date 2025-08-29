2d919564af346d4aee5ca1d9dd176bd8e4ea9fd3
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-08-29 21:05:13 +0530

    Added(error.rs): thisError implementation

--- v1/src/errors.rs
+++ v1/src/errors.rs
@@ -1,30 +1,60 @@
 use pinocchio::program_error::ProgramError;
+use solana_program::{decode_error::DecodeError, msg, program_error::PrintProgramError};
+use thiserror::Error;
 
-#[derive(Debug, Clone, PartialEq, shank::ShankType)]
+#[derive(Debug, Clone, PartialEq, shank::ShankType, Error)]
 pub enum SolanaCoreError {
+    #[error("Invalid instruction data")]
     InvalidInstructionData,
+
+    #[error("PDA mismatch")]
     PdaMismatch,
+
+    #[error("Invalid account owner")]
     InvalidOwner,
+
+    #[error("Math error")]
     MathError,
+
+    #[error("Not enough account keys")]
     NotEnoughAccountKeys,
+
+    #[error("Pool account not writable")]
     PoolAccountNotWritable,
+
+    #[error("Signer required")]
     SignerRequired,
+
+    #[error("Invalid account data")]
     InvalidAccountData,
+
+    #[error("Account already initialized")]
     AccountAlreadyInitialized,
+
+    #[error("Missing required signature")]
     MissingRequiredSignature,
+
+    #[error("Overflow detected")]
     OverFlowDetected,
+
+    #[error("Slippage error")]
     Slippage,
+}
+
+impl PrintProgramError for SolanaCoreError {
+    fn print<E>(&self) {
+        msg!(&self.to_string());
+    }
 }
 
 impl From<SolanaCoreError> for ProgramError {
     fn from(e: SolanaCoreError) -> Self {
-        Self::Custom(e as u32)
+        ProgramError::Custom(e as u32)
     }
 }
 
-impl From<ProgramError> for SolanaCoreError {
-    fn from(_e: ProgramError) -> Self {
-        // For simplicity, we'll map all ProgramErrors to InvalidInstructionData
-        SolanaCoreError::InvalidInstructionData
+impl<T> DecodeError<T> for MetadataError {
+    fn type_of() -> &'static str {
+        "Metadata Error"
     }
 }
